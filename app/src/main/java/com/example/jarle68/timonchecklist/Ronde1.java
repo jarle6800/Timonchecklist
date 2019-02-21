d1a1560157d10fff7f6a9d91228abb7ea209d67e
jarle6800 <j.t.toussaint@student.tudelft.nl>
2019-02-21 11:56:52 +0100

    added menu to Ronde1

--- app/src/main/java/com/example/jarle68/timonchecklist/Ronde1.java
+++ app/src/main/java/com/example/jarle68/timonchecklist/Ronde1.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -33,6 +35,32 @@ public class Ronde1 extends AppCompatActivity {
         });
 
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.contact) {
+
+            Intent intent = new Intent(this,Contact.class);
+            this.startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
 
     }
 
