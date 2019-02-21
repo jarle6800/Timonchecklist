2d0727351890e6e2a0987d238bfa0988734249cb
jarle6800 <j.t.toussaint@student.tudelft.nl>
2019-02-21 11:57:32 +0100

    added menu to Ronde2

--- app/src/main/java/com/example/jarle68/timonchecklist/Ronde2.java
+++ app/src/main/java/com/example/jarle68/timonchecklist/Ronde2.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -28,6 +30,32 @@ public class Ronde2 extends AppCompatActivity {
                 startActivity(new Intent(Ronde2.this, Main2Activity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.contact) {
+
+            Intent intent = new Intent(this,Contact.class);
+            this.startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+
     }
 
     public void checkBoxes()
